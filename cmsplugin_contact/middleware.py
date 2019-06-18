864d0f290eb3c666e18d2afd17ed410216de829d
Wei Jian Gan <weijiangan@outlook.com>
2019-06-18 14:41:14 +0800

    use deprecation.MiddlewareMixin for middleware

--- cmsplugin_contact/middleware.py
+++ cmsplugin_contact/middleware.py
@@ -1,7 +1,9 @@
 
-class ForceResponseMiddleware:
+from django.utils.deprecation import MiddlewareMixin
 
+
+class ForceResponseMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        if getattr(request, 'django_cms_contact_redirect_to', None) :
+        if getattr(request, 'django_cms_contact_redirect_to', None):
             return request.django_cms_contact_redirect_to
         return response
