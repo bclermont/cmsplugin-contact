e1ed94809cf097f74a031c6f669406f67321568c
Bruno Clermont <bruno@blabs.asia>
2019-11-06 16:06:37 +0800

    fix typo

--- cmsplugin_contact/cms_plugins.py
+++ cmsplugin_contact/cms_plugins.py
@@ -177,7 +177,7 @@ class ContactPlugin(CMSPluginBase):
         form = self.create_form(instance, request)
         self.render_template = getattr(form, 'template', self.render_template)
         if request.method == "POST" and form.is_valid():
-            send = getattr(form, "send", None):
+            send = getattr(form, "send", None)
             if send is None:
                 self.send(form, instance.form_name, instance.site_email, attachments=request.FILES)
             else:
